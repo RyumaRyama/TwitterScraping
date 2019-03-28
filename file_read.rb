5d15fdb9eae529291fc5e6df41087a9225cf38f5
RyumaRyama <homes.ryu@gmail.com>
2019-03-28 23:43:36 +0900

    print min_position

--- file_read.rb
+++ file_read.rb
@@ -1,6 +1,12 @@
-f = File.open("./json.json")
-text = f.read
-f.close
-text.split('\n').each do |word|
-  puts word.gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }
+def get_next_page_json(text)
+  $1 if text =~ /data-min-position="(.+?)"/
 end
+
+f = File.open("./wassyoi.html")
+text = f.read
+p get_next_page_json(text)
+f.close
+
+# text.split('\n').each do |word|
+#   puts word.gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }
+# end
