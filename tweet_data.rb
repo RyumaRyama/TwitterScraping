2c35713b69048e5d51a3d5cfc5fbb28a0e256bb6
Ryuma Maeshiro <ryama@RyumanoMacBook-Pro.local>
2019-04-14 00:15:56 +0900

    replace emoticons with plain characters

--- tweet_data.rb
+++ tweet_data.rb
@@ -1,12 +1,20 @@
 class TweetData
 
   def initialize(html)
-    # tweetのデータが入っているlistを正規表現で切り取り
+    # アカウント名を取得
     @account = html[/data-screen-name="(.+?)"/, 1]
+
+    # tweet日時を取得
     @time_and_day = html[%r{<small class="time">(.+?)</small>}m][/title="(.+?)"/, 1]
+
+    # tweet内容のみを抽出
     @tweet = html[%r{<div class="js-tweet-text-container">(.+?)</div>}m][%r{<p.+?>(.+?)</p>}m, 1]
-    if @tweet =~ %r{(.*?)<a.+</a>}
-      @tweet = $1
+    # 画像があればリンクを削除
+    @tweet = $1 if @tweet =~ %r{(.*?)<a.+</a>}
+    # 絵文字なども削除
+    if @tweet =~ /<img .+? title="(.+?)" .+?>/
+      img = "[" + $1 + "]"
+      @tweet = @tweet.gsub(/<img.+?>/, img)
     end
   end
 
