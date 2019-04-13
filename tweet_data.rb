11a287f786111d72b69df9cbaa73387e1adc40fe
Ryuma Maeshiro <ryama@RyumanoMacBook-Pro.local>
2019-04-13 23:32:10 +0900

    remove image url

--- tweet_data.rb
+++ tweet_data.rb
@@ -5,6 +5,9 @@ class TweetData
     @account = html[/data-screen-name="(.+?)"/, 1]
     @time_and_day = html[%r{<small class="time">(.+?)</small>}m][/title="(.+?)"/, 1]
     @tweet = html[%r{<div class="js-tweet-text-container">(.+?)</div>}m][%r{<p.+?>(.+?)</p>}m, 1]
+    if @tweet =~ %r{(.*?)<a.+</a>}
+      @tweet = $1
+    end
   end
 
   def account
